b4921545ce9c39264269411ca769fe40b92d61c3
Viny <viniciusfernandes1357@gmail.com>
2023-12-01 10:58:59 -0300

    Comt do Viny

--- calc.py
+++ calc.py
@@ -15,12 +15,15 @@ def divisao(v1, v2):
 def elevar_potencia(v1, v2):
      return v1 ** v2
 
+def resto_inteiro(v1, v2):
+    return v1 % v2
 
 # Teste
 
-# print(f'{3} ** {3} = {elevar_potencia(3,3)}') 
+# print(f'{3} ^ {3} = {elevar_potencia(3,3)}')
 # print(f'{2} + {2} = {soma(2,2)}')
 # print(f'{2} - {5} = {subtração(2,5)}')
 
 # print(f'{2} x {2} = {mult(2,2)}')
 #print(f'{10} / {5} = {divisao(10,5)}')
+# print(f'{5} % {2} = {resto_inteiro(5,2)}')
