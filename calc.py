72d737a5f725ba06813ae048e4eecf7d240f2052
felippecardoso7 <felippecardoso54@gmail.com>
2023-12-01 08:51:53 -0200

    Comit do Tio

--- calc.py
+++ calc.py
@@ -3,7 +3,12 @@
 def soma(v1, v2):
     return v1 + v2
 
+def subtração(v1, v2):
+    return    v1 - v2
+
+
+
 # Teste
 
 # print(f'{2} + {2} = {soma(2,2)}')
-
+# print(f'{2} - {5} = {subtração(2,5)}')
