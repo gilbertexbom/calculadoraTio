47a07c7cbf59d80e510baadf8ac37bc60dcb4349
TioLaba <151744456+TioLaba@users.noreply.github.com>
2023-12-01 09:52:38 -0300

    Add files via upload
    aqui esta o meu mestre

---
+++ calcmulti.py
@@ -0,0 +1,21 @@
+# calculadora simples
+def soma(v1, v2):
+     return v1 + v2
+
+def multi(v1,v2):
+     return v1 * v2
+def divisao(v1, v2):
+     return v1 / v2
+
+def subtração(v1, v2):
+     return v1 - v2
+
+ # teste
+
+#print(f'{2} - {2} = {subtração(2, 2)}')
+
+#print(f'{2} / {2} = {divisao(2,2)}')
+
+#print(f'{2} + {2} = {soma(2,2)}')
+
+#print(f'{2} * {3} = {multi(2,3)}')
---
+++ interface calculadora.py
@@ -0,0 +1,11 @@
+
+from calcmulti import soma
+
+print('\n\t\t\t calculadora multiplicação \n')
+
+n1= int(input('valor 1'))
+n2= int(input('valor 2'))
+
+total = soma(n1, n2)
+
+print(f'{n1} + {n2} = {total}:')
