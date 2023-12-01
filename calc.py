a6a24bf58ac5f8d86e9ab167d0c9257ef3bcf33d
Aluno <luhaya@yahoo.com.br>
2023-12-01 09:46:48 -0300

    Commit da Lucy

--- calc.py
+++ calc.py
@@ -8,9 +8,16 @@ def subtração(v1, v2):
 
 def mult(v1, v2):
     return v1 * v2
+
+def divisao(v1, v2):
+    return v1 / v2
+
+
+
 # Teste
 
 # print(f'{2} + {2} = {soma(2,2)}')
 # print(f'{2} - {5} = {subtração(2,5)}')
 
 # print(f'{2} x {2} = {mult(2,2)}')
+#print(f'{10} / {5} = {divisao(10,5)}')
